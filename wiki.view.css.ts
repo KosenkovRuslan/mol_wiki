56fceb76c60e60c882d2433233f4f2d4b0cecdfb
Ruslan Kosenkov <rkosenkov@kvant-is.ru>
2024-06-06 14:55:33 +0300

    Single page with local storage synchronization

---
+++ wiki.view.css.ts
@@ -0,0 +1,11 @@
+namespace $ {
+
+	$mol_style_define( $project2_wiki, {
+		
+		Body: {
+			padding: $mol_gap.block,
+		},
+
+	} )
+
+}
---
+++ wiki.view.ts
@@ -0,0 +1,8 @@
+namespace $.$$ {
+	export class $project2_wiki extends $.$project2_wiki {
+
+		text( next?: string) {
+			return this.$.$mol_state_local.value(`text`, next) ?? ''
+		}
+	}
+}
